a663fb29c99e80e465d649815199d6b6f287d174
Ruby <pineapples1104@gmail.com>
2025-01-09 14:15:48 -0700

    day 1

--- W01/demo.js
+++ W01/demo.js
@@ -5,3 +5,66 @@
 // fetch
 // DOM and events
 
+let myNum = 1;
+
+if (myNum == "1"); // would return true, coerces the string to a number
+
+if (myNum === "1"); // would return false, strict comparison
+
+double(2);
+//double2(2); // causes an error because it is not hoisted to the top with it being a function expression
+
+
+// function declaration
+function double(x) {
+    return x * 2;
+}
+
+
+// function expression
+const double2 = function (x) { //anonymous function
+    return x * 2;
+}
+
+
+// arrow function
+const double3 = (x) => {
+    return x * 2;
+}
+
+const double4 = x => x * 2;
+
+const myList = [1, 2, 3, 4];
+
+function doubleList(list) {
+    const doubled = [];
+    list.forEach((item) => {
+        doubled.push(item * 2)
+    });
+    return doubled;
+}
+
+// callbacks
+function modifyList(list, action){
+    const modified = [];
+    list.forEach((item) => {
+        modified.push(action(item));
+    });
+    return modified;
+}
+
+const newList = modifyList(myList, double4);
+
+const newList2 = myList.map(double4); // same thing as line 55
+
+// fetch
+
+const baseURL =  "https://pokeapi.co/api/v2/pokemon/";
+
+async function getData(url){
+    const response = await fetch(baseURL + url);
+    console.log(response);
+    return response.json();
+}
+
+getData("pikachu");
